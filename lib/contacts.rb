cc92963de60a1a1be809f03bca5bef3e3b32a195
Anysia V <anysiavalko@gmail.com>
2018-11-08 22:43:27 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -20,7 +20,7 @@ def remove_strawberry(contacts)
       if attribute == :favorite_ice_cream_flavors
         value.each do |flavor|
           if flavor == "strawberry"
-            attribute.flavor.
+            attribute.delete(flavor)
           end
         end
       end
